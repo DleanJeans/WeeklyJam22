98690e62141653b7d438cdaa3e9034d73ee03e96
Dlean Jeans <dleanjeans@gmail.com>
2017-12-19 08:04:42 +0700

    CoinSpawner: Fix random spawning position math

--- godot/CoinSpawner.gd
+++ godot/CoinSpawner.gd
@@ -3,7 +3,8 @@ extends Area2D
 export(NodePath) var container_path = "../../Objects"
 export(float) var spawn_interval = 3
 
-onready var size = $Shape.shape.extents * 2
+onready var scaled_extents = $Shape.shape.extents * scale
+onready var size = scaled_extents * 2
 onready var container = get_node(container_path)
 
 var Coin = preload("res://Coin.tscn")
@@ -19,11 +20,17 @@ func resume():
 	$Timer.start()
 
 func spawn():
-	var random_position = Vector2(rand_range(0, size.x), rand_range(0, size.y))
+	var random_position = Vector2(_random_x(), _random_y())
 	var coin = Coin.instance()
-	coin.position = position - $Shape.shape.extents + random_position
 	
 	container.add_child(coin)
+	coin.position = position - scaled_extents + random_position
+
+func _random_x():
+	return randi() % int(size.x)
+
+func _random_y():
+	return randi() % int(size.y)
 
 func clear_coins():
 	for c in get_tree().get_nodes_in_group("Coins"):
