5e7edabef28312843720fbae21df45f02d6e06a5
Dlean Jeans <dleanjeans@gmail.com>
2018-01-18 15:31:54 +0700

    Coin: Change type to KinematicBody2D

--- godot/source/Coin.gd
+++ godot/source/Coin.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends KinematicBody2D
 
 export(int) var magnet_speed = 100
 var collected = false
@@ -16,18 +16,24 @@ func _on_collected(body):
 func _is_not_player(body):
 	return not body is Classes.Player
 
-func _on_animation_finished(_name):
-	if _name == "Collected":
-		queue_free()
-
 func _physics_process(delta):
+	_work_magnet()
+
+func _work_magnet():
 	var bodies = $MagnetRadius.get_overlapping_bodies()
 	for body in bodies:
 		if not body in Global.Players: continue
 		
-		var to_player = (body.position - position).normalized()
-		var movement = to_player * delta * magnet_speed
-		
-		if body.is_crocodile():
-			position += -movement
-		else: position += movement
+		_magnet_player(body)
+
+func _magnet_player(player):
+	var to_player = Utility.unit_vector_from(self, player)
+	var velocity = to_player * magnet_speed
+	
+	if player.is_crocodile():
+		move_and_slide(-velocity)
+	else: move_and_slide(velocity)
+
+func _on_screen_exited():
+	queue_free()
+	print("Coin out of screen! Free!")
