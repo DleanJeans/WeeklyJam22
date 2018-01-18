eaac6bc5e45b9c8c3baddf3c146c525edacc6296
Dlean Jeans <dleanjeans@gmail.com>
2018-01-18 16:14:15 +0700

    Add a CoinSpawner to TheGator

--- godot/source/CoinSpawner.gd
+++ godot/source/CoinSpawner.gd
@@ -1,11 +1,15 @@
 extends Area2D
 
-export(NodePath) var container_path = "../../Objects"
-export(float) var spawn_interval = 3
+signal coin_spawned(coin)
 
-onready var scaled_extents = $Shape.shape.extents * scale
-onready var size = scaled_extents * 2
-onready var container = get_node(container_path)
+var Coin = load("res://source/Coin.tscn")
+
+export(float) var spawn_interval = 1
+export(float) var fluctuating_amount = 0.1
+
+onready var _coin_parent
+onready var _scaled_extents = $Shape.shape.extents * scale
+onready var _size = _scaled_extents * 2
 
 func _ready():
 	$Timer.start()
@@ -22,16 +26,20 @@ func spawn():
 
 func _spawn_coin():
 	var random_position = Vector2(_random_x(), _random_y())
-	var coin = load("res://source/Coin.tscn").instance()
+	var coin = Coin.instance()
 	
-	container.add_child(coin)
-	coin.position = position - scaled_extents + random_position
+	coin.position = global_position - _scaled_extents + random_position
+	_coin_parent.add_child(coin)
+	emit_signal("coin_spawned", coin)
 
 func _random_x():
-	return randi() % int(size.x)
+	return randi() % int(_size.x)
 
 func _random_y():
-	return randi() % int(size.y)
+	return randi() % int(_size.y)
 
 func _flutuate_spawn_time():
-	$Timer.wait_time = spawn_interval + rand_range(-0.5, 0.5)
+	$Timer.wait_time = spawn_interval + rand_range(-fluctuating_amount, fluctuating_amount)
+
+func set_coin_parent(value):
+	_coin_parent = value
--- godot/source/TheGator.gd
+++ godot/source/TheGator.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+export(NodePath) var coin_parent_path = "../Map/Objects"
+export(int) var distance_between_coins = 100
+
 onready var Gator = $Gator
 
-var crocodile
+var _crocodile
+var _last_coin_spawn_position
 
 func reparent_here():
 	reparent(self)
@@ -19,16 +23,61 @@ func _ready():
 	Global.connect("crocodile_changed", self, "_on_crocodile_changed")
 
 func _on_crocodile_changed():
-	crocodile = Global.crocodile
-	if crocodile == null:
+	_crocodile = Global.crocodile
+	
+	if _no_crocodile():
+		_stop_coin_spawner()
 		hide()
 		return
-	else:
-		show()
+	else: show()
 	
 	_listen_to_crocodile_going_rawr()
 	reparent_to_crocodile()
+	_stop_for_awhile()
+
+func _no_crocodile():
+	return _crocodile == null
+
+func _stop_coin_spawner():
+	$CoinSpawner.stop()
 
 func _listen_to_crocodile_going_rawr():
-	if not crocodile.is_connected("rawr", Gator, "rawr"):
-		crocodile.connect("rawr", Gator, "rawr")
+	if not _crocodile.is_connected("rawr", Gator, "rawr"):
+		_crocodile.connect("rawr", Gator, "rawr")
+
+func _stop_for_awhile():
+	$CoinSpawner.stop()
+	yield(Utility.timer(3), "timeout")
+	if Global.crocodile != null:
+		$CoinSpawner.resume()
+
+func _process(delta):
+	_update_position()
+	_spawn_coin_if_go_far_enough()
+
+func _update_position():
+	if Gator.get_parent() != self:
+		position = Gator.global_position
+
+func _spawn_coin_if_go_far_enough():
+	if _last_coin_spawn_position == null:
+		_spawn_coin()
+		return
+	
+	var distance_squared_to_last_spawn_point = _get_distance_squared_to_last_spawn_point()
+	var distance_between_coins_squared = distance_between_coins * distance_between_coins
+	if distance_squared_to_last_spawn_point > distance_between_coins_squared:
+		_spawn_coin()
+
+func _get_distance_squared_to_last_spawn_point():
+	return Utility.distance_squared(position, _last_coin_spawn_position)
+
+func _spawn_coin():
+	$CoinSpawner.spawn()
+
+func update_coin_parent():
+	var coin_parent = get_node(coin_parent_path)
+	$CoinSpawner.set_coin_parent(coin_parent)
+
+func _on_coin_spawned(coin):
+	_last_coin_spawn_position = coin.global_position
